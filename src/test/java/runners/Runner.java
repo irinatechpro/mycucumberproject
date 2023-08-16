98a42d6363df770e99bbd83565c8cf7ae2958e9e
Iryna R <mnops@ukr.net>
2023-08-16 17:05:32 +0100

    day 21

--- src/test/java/runners/Runner.java
+++ src/test/java/runners/Runner.java
@@ -4,6 +4,15 @@ import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @CucumberOptions(
+
+        plugin = {
+                        "pretty",//generate the report on console colorfully
+                "html:target/default-cucumber-reports.html",
+                "json:target/json-reports/cucumber.json",
+                "junit:target/xml-report/cucumber.xml"
+
+        },
+        //monochrome = true,// it makes the report on console readable
         features = "./src/test/resources",//path of feature file
         glue = "stepdefinitions", //path of the step definitions
         tags = "@data_tables",//this marks which feature file or scenario to run.
